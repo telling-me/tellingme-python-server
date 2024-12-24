4cf58891c7bce4b16202354ad6f50b7138843b1a
taewoo-dev <twin9774@gmail.com>
2024-12-24 09:10:45 +0900

    🐛 fix: 좋아요 경험치 지급 미션 버그 수정

--- src/app/v2/likes/querys/like_query.py
+++ src/app/v2/likes/querys/like_query.py
@@ -2,5 +2,6 @@ SELECT_UNIQUE_LIKES_COUNT_BY_USER_TODAY_QUERY = """
     SELECT COUNT(DISTINCT answer_id) AS unique_likes
     FROM likes
     WHERE user_id = UNHEX(REPLACE(%s, '-', ''))
-      AND DATE(created_time) = CURDATE();
+      AND created_time >= DATE_ADD(CURDATE(), INTERVAL 6 HOUR)
+      AND created_time < DATE_ADD(CURDATE(), INTERVAL 30 HOUR);
 """
