c0616b0a7f241c57fea55cae4bb6178e3b79e2bb
taewoo-dev <twin9774@gmail.com>
2025-06-12 22:05:31 +0900

    ✅ test: add cheese router

--- app/tests/telling_me_client.py
+++ app/tests/telling_me_client.py
@@ -57,6 +57,18 @@ class TellingMeClient:
             },
         )
 
+    async def get_cheese_amount(self, user_id: str) -> httpx.Response:
+        return await self._client.get(
+            "/api/v2/cheese",
+            params={
+                key: value
+                for key, value in {
+                    "user_id": user_id,
+                }.items()
+                if value is not None
+            },
+        )
+
     # async def create_product(self, token: str, create_product_request: CreateProductRequest) -> httpx.Response:
     #     return await self._client.post(
     #         "/v1/products/admin", json=create_product_request.model_dump(), headers={"Authorization": f"Bearer {token}"}
